c1fae105f558625896a4cba20d510bb03e3ddc52
Gustavo Augusto Palmeira Oliveira <gugaapo@gustavo-debian.mshome.net>
2025-10-09 18:39:10 -0400

    Initial commit: Alacritty(theme) + Oh My Bash(theme + general aliases) + past script basic config

---
+++ oh-my-bash-custom/aliases/general.aliases.sh
@@ -0,0 +1,8 @@
+alias edit_alias='cd $HOME/.oh-my-bash/custom/aliases'
+alias bash_update='source ~/.bashrc'
+alias bash_edit='nano ~/.bashrc'
+alias cd..='cd ..'
+alias cd.='cd.'
+alias e='exit'
+alias nano='nano -l -T 4'
+
